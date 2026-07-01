80f00a50f6086b3b663dd13f7f78e039e09d384f
BROHIT-cmd <Bharat.rohit@xylem.com>
2026-07-01 13:58:30 +0530

    Update step_utils.py

--- step_utils.py
+++ step_utils.py
@@ -1,20 +1,6 @@
-from OCC.Core.STEPControl import STEPControl_Reader
-from OCC.Core.BRepGProp import brepgprop_VolumeProperties
-from OCC.Core.GProp import GProp_GProps
+import cadquery as cq
 
 def calculate_volume(step_file_path):
-    reader = STEPControl_Reader()
-    status = reader.ReadFile(step_file_path)
-
-    if status != 1:
-        raise Exception("Error reading STEP file")
-
-    reader.TransferRoots()
-    shape = reader.OneShape()
-
-    props = GProp_GProps()
-    brepgprop_VolumeProperties(shape, props)
-
-    volume = props.Mass()  # cubic units
-
+    shape = cq.importers.importStep(step_file_path)
+    volume = shape.val().Volume()
     return volume
