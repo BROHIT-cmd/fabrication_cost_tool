7fd69b752e9cd25528b1895a3c63a18921cbe513
BROHIT-cmd <Bharat.rohit@xylem.com>
2026-07-01 14:00:27 +0530

    Update step_utils.py

--- step_utils.py
+++ step_utils.py
@@ -1,7 +1,19 @@
 import cadquery as cq
 
 def calculate_volume(step_file_path):
-    shape = cq.importers.importStep(step_file_path)
-    volume = shape.val().Volume()
-    return volume
+    try:
+        shape = cq.importers.importStep(step_file_path)
 
+        # cadquery objects may contain multiple solids
+        volumes = []
+
+        for solid in shape.solids().vals():
+            volumes.append(solid.Volume())
+
+        total_volume = sum(volumes)
+
+        return total_volume
+
+    except Exception as e:
+        raise Exception(f"STEP processing failed: {str(e)}")
+``
